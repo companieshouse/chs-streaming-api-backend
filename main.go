15d3e4e7387decb1793896f2595590d4099e5915
dgroves-ch <dgroves@companieshouse.gov.uk>
2020-10-06 08:45:43 +0100

    Add main function
    * Preliminary healthcheck endpoint implementation

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	backend_config "github.com/companieshouse/chs-streaming-api-backend/config"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config, err := backend_config.Get()
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(200)
+	})
+	log.Fatal(http.ListenAndServe(":" + config.BindAddress, nil))
+}
