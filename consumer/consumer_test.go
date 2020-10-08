6f6976fffa1c8c7d49e88ca864e8253e51f24e78
dgroves-ch <dgroves@companieshouse.gov.uk>
2020-10-08 14:08:04 +0100

    Fix mock logger

--- consumer/consumer_test.go
+++ consumer/consumer_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/companieshouse/chs.go/log"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/mock"
+	"net/http"
 	"sync"
 	"syscall"
 	"testing"
@@ -206,4 +207,8 @@ func (l *mockLogger) Error(err error, data ...log.Data) {
 
 func (l *mockLogger) Info(msg string, data ...log.Data) {
 	l.Called(msg, data)
+}
+
+func (l *mockLogger) InfoR(req *http.Request, msg string, data ...log.Data) {
+	l.Called(req, msg, data)
 }
